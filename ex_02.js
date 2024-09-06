9817df2b8ad0f95ed86a150fafd742180635d3d1
EM02-AL <eddy.mazi@epitech.digital>
2024-09-06 15:42:47 +0200

    Update ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,1 +1,13 @@
+const object = {
+    name : "agent"
+}; 
+const agentString = JSON.stringify(object); 
+const car = "Aston Martin";
 
+    function addCar (agentString, car) {
+        const object = JSON.parse(agentString);
+        object.car = car; 
+        return object
+    }
+ 
+console.log(addCar(agentString, car));
